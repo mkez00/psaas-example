274b96a6b716a4ebaf5c6f2ee8552a46c2d59ceb
Michael Kezele <michael.kezele@scenedoc.com>
2018-08-07 11:14:55 -0400

    reload nginx, don't restart the service

--- data/psaas-cli.sh
+++ data/psaas-cli.sh
@@ -3,12 +3,12 @@
 if [ $1 = "update-port" ]; then
 	echo "Updating Port"
 	sed -i '/listen/c\listen '"$2"';' /etc/nginx/sites-available/default
-	systemctl restart nginx
+	nginx -s reload
 	echo "Port Updated Successfully"
 elif [ $1 = "disable-ssl" ]; then
 	echo "Disabling SSL"
 	sed -i '/ssl/c\' /etc/nginx/sites-available/default
-	systemctl restart nginx
+	nginx -s reload
 	echo "SSL Disabled Successfully"
 else
 	echo "No option specified.  Options available:"
